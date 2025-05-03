6c35226de5c997b46e5abe14e4bb129fa040edad
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-05-03 15:37:32 +0900

    [react-charts-practice]feat: add Header and set up routing

--- react-charts-practice/src/main.tsx
+++ react-charts-practice/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Header from "./components/Header.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+  // </StrictMode>
 );
